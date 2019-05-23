3c6c68671b51e56f33cd7c03085a105a89612985
Calin Culianu <calin.culianu@gmail.com>
2019-05-23 09:36:13 +0300

    Initial Commit

---
+++ osxqrdetect/osxqrdetect.m
@@ -0,0 +1,13 @@
+//
+//  osxqrdetect.m
+//  osxqrdetect
+//
+//  Created by calin on 5/23/19.
+//  Copyright © 2019 electroncash.org. All rights reserved.
+//
+
+#import "osxqrdetect.h"
+
+@implementation osxqrdetect
+
+@end
